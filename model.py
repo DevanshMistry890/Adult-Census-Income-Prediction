8ceb17a3b8ea96a4447e3eef46dbc96e152ec47c
Devansh Mistry <73028874+DevanshMistry890@users.noreply.github.com>
2022-06-29 19:07:37 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -1,20 +1,41 @@
-# importing the dataset
-import pandas
+# This file is showing how model process data form raw to training,
+# for modelling there is saperate file
+# importing Required libraries
+import logging
 import numpy
-from sklearn import preprocessing
+import pandas
+import imblearn
 import pickle
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from collections import Counter
+from imblearn.over_sampling import SMOTE
+import lightgbm as lgb
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
+# log file initialization 
+logging.basicConfig(filename='test.log', level=logging.DEBUG,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
+logging.debug(' Model.py File execution started ')
+
+# loading database with pandas library
 df = pandas.read_csv('adult.csv')
-df.head()
+logging.debug(' Database Loaded ')
 
+# dropping unnecessary and redundant  columns
 df = df.drop(['fnlwgt', 'education-num'], axis=1)
 
+# handling missing values in df
 col_names = df.columns
-
 for c in col_names:
 	df = df.replace("?", numpy.NaN)
 df = df.apply(lambda x: x.fillna(x.value_counts().index[0]))
 
+
+# Discretization
 df.replace(['Divorced', 'Married-AF-spouse',
 			'Married-civ-spouse', 'Married-spouse-absent',
 			'Never-married', 'Separated', 'Widowed'],
@@ -23,8 +44,15 @@ df.replace(['Divorced', 'Married-AF-spouse',
 
 category_col = ['workclass', 'race', 'education', 'marital-status', 'occupation',
 				'relationship', 'sex', 'country', 'salary']
+
+# labelEncoding
 labelEncoder = preprocessing.LabelEncoder()
 
+# mapping 
+"""
+	mapping categorical variables to numerical value under labelencoder
+	return dictionary   
+""" 
 mapping_dict = {}
 for col in category_col:
 	df[col] = labelEncoder.fit_transform(df[col])
@@ -33,45 +61,43 @@ for col in category_col:
 							labelEncoder.transform(labelEncoder.classes_)))
 
 	mapping_dict[col] = le_name_mapping
-print(mapping_dict)
 
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
+logging.debug(' Database Pre-Processing Done ')
 
+# model featuring
 X = df.values[:, 0:12]
 Y = df.values[:, 12]
 
-X_train, X_test, y_train, y_test = train_test_split(
-		X, Y, test_size = 0.3, random_state = 100)
+# Data Spliting For model training
+X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=0.30)
 
-dt_clf_gini = DecisionTreeClassifier(criterion = "gini",
-									random_state = 100,
-									max_depth = 5,
-									min_samples_leaf = 5)
+# summarize class distribution
+print("Before oversampling: ",Counter(y_train))
 
-dt_clf_gini.fit(X_train, y_train)
-y_pred_gini = dt_clf_gini.predict(X_test)
+# define oversampling strategy
+SMOTE = SMOTE()
 
-print ("Decision Tree Accuracy is ",
-			accuracy_score(y_test, y_pred_gini)*100 )
+# fit and apply the transform
+X_train_SMOTE, y_train_SMOTE = SMOTE.fit_resample(X_train, y_train)
 
-from sklearn.ensemble import ExtraTreesClassifier
-# Building the model
-extra_tree_forest = ExtraTreesClassifier(n_estimators = 5,criterion ='entropy', max_features = 2)
-extra_tree_forest.fit(X_train, y_train)
+# summarize class distribution
+print("After oversampling: ",Counter(y_train_SMOTE))
 
-predictions_e = extra_tree_forest.predict(X_test)
-print('Extra_tree_forest Accuracy: ', accuracy_score(y_test, predictions_e)*100)
+# model fitting using LGBMClassifier
+clf = lgb.LGBMClassifier()
+clf.fit(X_train_SMOTE, y_train_SMOTE)
 
 
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.model_selection import KFold
-kf = KFold(n_splits=5,random_state=250,shuffle=True)
-gradient_booster = GradientBoostingClassifier(learning_rate=0.1)
-gradient_booster.get_params()
-gradient_booster.fit(X_train,y_train)
-predictions_g = gradient_booster.predict(X_test)
-print('gradient_booster Accuracy: ', accuracy_score(y_test, predictions_g)*100)
+# Show the Training and Testing Data
+print('Shape of training feature:', X_train_SMOTE.shape)
+print('Shape of testing feature:', X_test.shape)
+print('Shape of training label:', y_train_SMOTE.shape)
+print('Shape of training label:', y_test.shape)
 
-pickle.dump(gradient_booster, open('model.pkl','wb'))
+# Printing Accuracy
+predictions_e = clf.predict(X_test)
+print('Accuracy: ', accuracy_score(y_test, predictions_e))
+
+# pkl export & finish log
+pickle.dump(clf, open('model.pkl','wb'))
+logging.debug(' Execution of Model.py is finished ')
